f4aeb6d3db557e52ad65a52ee60be133890fdbc8
Vigas <77221759+vigasselvan@users.noreply.github.com>
2025-10-29 20:41:37 +0530

    add files to the repo
    created websites files to code

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const container = document.getElementById("container");
+
+for(let i = 0; i < 256; i++){
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("gridSquare");
+}
