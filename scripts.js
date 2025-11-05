5d9610763f415077d787d2bacfe2d25cc8324941
Vigas <77221759+vigasselvan@users.noreply.github.com>
2025-11-05 16:55:30 +0530

    add hover drawing effect
    to create a  pencil drwaing like effect using hover

--- scripts.js
+++ scripts.js
@@ -3,4 +3,8 @@ const container = document.getElementById("container");
 for(let i = 0; i < 256; i++){
     const gridSquare = document.createElement("div");
     gridSquare.classList.add("gridSquare");
+    container.appendChild(gridSquare);
+    gridSquare.addEventListener("mouseout", (event)=>{
+        event.target.style.backgroundColor = 'green';
+    });
 }
