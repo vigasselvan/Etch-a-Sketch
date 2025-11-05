3a7050fe49be7c0cea26d4b64b63764e28d18b00
Vigas <77221759+vigasselvan@users.noreply.github.com>
2025-11-05 18:20:02 +0530

    add set divs size feature
    add div size feature, reload feature, different colors when hovered.

--- scripts.js
+++ scripts.js
@@ -5,6 +5,49 @@ for(let i = 0; i < 256; i++){
     gridSquare.classList.add("gridSquare");
     container.appendChild(gridSquare);
     gridSquare.addEventListener("mouseout", (event)=>{
-        event.target.style.backgroundColor = 'green';
+        const randomRed = Math.floor(Math.random() * 255);
+        const randomgreen = Math.floor(Math.random() * 255);
+        const randomBlue = Math.floor(Math.random() * 255);
+        const color = `rgb(${randomRed}, ${randomgreen}, ${randomBlue})`;
+        event.target.style.backgroundColor = color;
     });
+}
+
+function pleaseReloadFirst(num){
+    //remove old divs
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    //add button first
+    const squareBtn = document.createElement("div");
+    squareBtn.setAttribute('id', 'sqPerSide');
+    squareBtn.setAttribute('onclick', 'giveSquare()');
+    squareBtn.textContent = "Squares per side";
+    container.appendChild(squareBtn);
+
+    //add divs
+    for(let i = 0; i < (num*num); i++){
+        const gridSquare = document.createElement("div");
+        gridSquare.classList.add("gridSquare");
+        container.appendChild(gridSquare);
+        gridSquare.style.height = (100/num)+'vh';
+        gridSquare.style.width = (100/num)+'vw'; 
+        gridSquare.addEventListener("mouseout", (event)=>{
+        const randomRed = Math.floor(Math.random() * 255);
+        const randomgreen = Math.floor(Math.random() * 255);
+        const randomBlue = Math.floor(Math.random() * 255);
+        const color = `rgb(${randomRed}, ${randomgreen}, ${randomBlue})`;
+        event.target.style.backgroundColor = color;
+    });
+    }
+}
+
+function giveSquare(){
+    let num = prompt("Enter Squares per side: ");
+    if(num > 100){
+        num = 100;
+    }
+    let val = 100/num;
+    val = val.toFixed(2);
+    pleaseReloadFirst(num); 
 }
